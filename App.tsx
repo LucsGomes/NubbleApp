ace7474859ac821b85959b61b088c51af903f348
Lucas Gomes <lucasgomesribeiroskt@hotmail.com>
2024-01-09 22:41:07 -0300

    feat: Design System with Restyle

--- App.tsx
+++ App.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, View} from 'react-native';
 import {Text} from './src/components/Text/Text';
+import {ThemeProvider} from '@shopify/restyle';
+
+import {theme} from './src/theme/theme';
+import {Button} from './src/components/Button/Button';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-      <Text preset="headingMedium">Coffstack</Text>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView style={{paddingHorizontal: 24}}>
+        <View>
+          <Text preset="headingLarge" italic>
+            Coffstack
+          </Text>
+          <Button title="Entrar" marginBottom="s24" />
+          <Button disabled title="Entrar" marginBottom="s24" />
+          <Button preset="outline" title="Outline" marginBottom="s24" />
+          <Button
+            disabled
+            preset="outline"
+            title="Outline"
+            marginBottom="s24"
+          />
+          <Button loading title="Loading" />
+        </View>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
