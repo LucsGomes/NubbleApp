9ffa21261205e52a70586abaa96cec34f2b5d9d7
Lucas Gomes <lucasgomesribeiroskt@hotmail.com>
2024-01-12 17:06:00 -0300

    feat: add TextInput Component

--- App.tsx
+++ App.tsx
@@ -6,27 +6,46 @@ import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 import {Button} from './src/components/Button/Button';
 import {Icon} from './src/components/Icon/Icon';
+import {Box} from './src/components/Box/Box';
+import {TextInput} from './src/components/TextInput/TextInput';
 
 function App(): React.JSX.Element {
   return (
     <ThemeProvider theme={theme}>
-      <SafeAreaView style={{paddingHorizontal: 24}}>
-        <View>
-          <Text preset="headingLarge" italic>
-            Coffstack
+      <SafeAreaView>
+        <View style={{paddingHorizontal: 24}}>
+          <Text marginBottom="s8" preset="headingLarge">
+            Olá
           </Text>
-          <Button title="Entrar" marginBottom="s24" />
-          <Button disabled title="Entrar" marginBottom="s24" />
-          <Button preset="outline" title="Outline" marginBottom="s24" />
-          <Button
-            disabled
-            preset="outline"
-            title="Outline"
-            marginBottom="s24"
-          />
-          <Button loading title="Loading" />
-          <Icon name="eyeOn" color="error" />
-          <Icon name="eyeOff" color="carrotSecondary" />
+
+          <Text preset="paragraphLarge" marginBottom="s40">
+            Digite seu e-mail e senha para entrar
+          </Text>
+
+          <Box>
+            <TextInput
+              errorMessage="Menssagem de erro"
+              label="E-mail"
+              placeholder="Digite seu e-mail"
+              boxProps={{mb: 's20'}}
+            />
+          </Box>
+
+          <Box>
+            <TextInput
+              label="Senha"
+              placeholder="Digite sua senha"
+              boxProps={{mb: 's10'}}
+              RightComponent={<Icon color="gray2" name="eyeOn" />}
+            />
+          </Box>
+
+          <Text color="primary" preset="paragraphSmall" bold>
+            Esqueci minha senha
+          </Text>
+
+          <Button mt="s48" title="Entrar" />
+          <Button mt="s12" preset="outline" title="Criar uma conta" />
         </View>
       </SafeAreaView>
     </ThemeProvider>
