c759493134bf2b391f583e075f6f9963defe2451
Lucas Gomes <lucasgomesribeiroskt@hotmail.com>
2024-01-13 22:37:46 -0300

    feat: navigation with react navigation

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -13,6 +13,7 @@ import {CameraIcon} from '../../assets/icons/CameraIcon';
 import {ChatIcon} from '../../assets/icons/ChatIcon';
 import {ChatOnIcon} from '../../assets/icons/ChatOnIcon';
 import {CheckIcon} from '../../assets/icons/CheckIcon';
+import {CheckRoundIcon} from '../../assets/icons/CheckRoundIcon';
 import {CommentIcon} from '../../assets/icons/CommentIcon';
 import {ChevronRightIcon} from '../../assets/icons/ChevronRightIcon';
 import {FlashOnIcon} from '../../assets/icons/FlashOnIcon';
@@ -22,6 +23,7 @@ import {HeartIcon} from '../../assets/icons/HeartIcon';
 import {HomeFillIcon} from '../../assets/icons/HomeFillIcon';
 import {HomeIcon} from '../../assets/icons/HomeIcon';
 import {MessageIcon} from '../../assets/icons/MessageIcon';
+import {MessageRoundIcon} from '../../assets/icons/MessageRoundIcon';
 import {NewPostIcon} from '../../assets/icons/NewPostIcon';
 import {ProfileFillIcon} from '../../assets/icons/ProfileFillIcon';
 import {ProfileIcon} from '../../assets/icons/ProfileIcon';
@@ -35,7 +37,7 @@ export interface IconBase {
   color?: string;
 }
 
-interface Props {
+export interface IconProps {
   name: IconNames;
   color?: ThemeColors;
   size?: number;
@@ -47,7 +49,7 @@ export function Icon({
   color = 'backgroundContrast',
   size,
   onPress,
-}: Props) {
+}: IconProps) {
   const {colors} = useAppTheme();
   const SVGIcon = iconRegistry[name];
 
@@ -72,6 +74,7 @@ const iconRegistry = {
   chat: ChatIcon,
   chatOn: ChatOnIcon,
   check: CheckIcon,
+  checkRound: CheckRoundIcon,
   comment: CommentIcon,
   chevronRight: ChevronRightIcon,
   eyeOn: EyeOnIcon,
@@ -83,6 +86,7 @@ const iconRegistry = {
   home: HomeIcon,
   homeFill: HomeFillIcon,
   message: MessageIcon,
+  messageRound: MessageRoundIcon,
   newPost: NewPostIcon,
   profile: ProfileIcon,
   profileFill: ProfileFillIcon,
--- src/screens/auth/LoginScreen/LoginScreen.tsx
+++ src/screens/auth/LoginScreen/LoginScreen.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import {Text} from '../../../components/Text/Text';
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {TextInput} from '../../../components/TextInput/TextInput';
 import {Box} from '../../../components/Box/Box';
 import {Button} from '../../../components/Button/Button';
 import {Screen} from '../../../components/Screen/Screen';
 import {PasswordInput} from '../../../components/PasswordInput/PasswordInput';
+import {RootStackParamList} from '../../../routes/Routes';
 
-export function LoginScreen() {
+type ScreenProps = NativeStackScreenProps<RootStackParamList, 'LoginScreen'>;
+export function LoginScreen({navigation}: ScreenProps) {
+  function navigateToSignUpScreen() {
+    navigation.navigate('SignUpScreen');
+  }
+  function navigateToForgotPasswordScreen() {
+    navigation.navigate('ForgotPasswordScreen');
+  }
   return (
     <Screen>
       <Text marginBottom="s8" preset="headingLarge">
@@ -34,12 +43,21 @@ export function LoginScreen() {
         />
       </Box>
 
-      <Text color="primary" preset="paragraphSmall" bold>
+      <Text
+        onPress={navigateToForgotPasswordScreen}
+        color="primary"
+        preset="paragraphSmall"
+        bold>
         Esqueci minha senha
       </Text>
 
       <Button mt="s48" title="Entrar" />
-      <Button mt="s12" preset="outline" title="Criar uma conta" />
+      <Button
+        onPress={navigateToSignUpScreen}
+        mt="s12"
+        preset="outline"
+        title="Criar uma conta"
+      />
     </Screen>
   );
 }
